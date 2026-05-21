7c632dd5afc8be99628c6505dcfd2f8bb755031c
RoucheOelofse <st10321098@vcconnect.edu.za>
2026-05-21 10:34:23 +0200

    fixed error

--- ChickenAPI/Model/Chicken.cs
+++ ChickenAPI/Model/Chicken.cs
@@ -10,7 +10,7 @@ namespace ChickenAPI.Model
 
         [Required]
         [MaxLength(50)]
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
         [Required]
         [MaxLength(50)]
